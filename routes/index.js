7280d0e9d05ef56993d489daa67479d7a00bbe89
Brian Jimerson <bjimerson@gopivotal.com>
2014-09-03 15:31:59 -0400

    Initial commit.

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+	var now = new Date();
+	var port = process.env.PORT;
+	var vcapApplication = JSON.parse(process.env.VCAP_APPLICATION);
+	var vcapServices = process.env.VCAP_SERVICES;
+	res.render('index', { title: 'EXAMPLE application for PCF', now: now.toLocaleString(), port: port, vcapApplication: vcapApplication, vcapServices: vcapServices });
+});
+
+module.exports = router;

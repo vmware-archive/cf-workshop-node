2641004d98880a0e84fa57566545fad05cb9451f
Brian Jimerson <bjimerson@gopivotal.com>
2014-09-03 18:22:40 -0400

    Added sessions view

--- routes/attendees.js
+++ routes/attendees.js
@@ -4,16 +4,16 @@ var router = express.Router();
 router.get('/list', function(req, res) {
 	var db = req.db;
 	var attendeeCollection = db.get('attendees');
-	attendeeCollection.find({}, {}, function(e, docs) {
-		res.render('attendees', {"attendees": docs});
+	attendeeCollection.find({}, {}, function(e, attendees) {
+		res.render('attendees', {"attendees": attendees});
 	});
 });
 
 router.get('/:attendee_id/sessions', function(req, res) {
 	var db = req.db;
 	var attendeeCollection = db.get('attendees');
-	attendeeCollection.findOne({_id: req.params.attendee_id}, {}, function(e, docs) {
-		res.render('sessions', {"sessions": docs});
+	attendeeCollection.findOne({_id: req.params.attendee_id}, {}, function(e, attendee) {
+		res.render('sessions', {"attendee": attendee});
 	});
 })
 
